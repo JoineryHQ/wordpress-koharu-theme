2f227b12d8825f26a282c66655b610685bcd439b
Allen Shaw <allen@JoineryHQ.com>
2026-03-18 18:32:08 -0500

    Show page title on non-front pages.

--- template-parts/components/main.php
+++ template-parts/components/main.php
@@ -12,6 +12,9 @@ if (have_posts()) {
     the_post();
     ?>
     <article <?php post_class('page-layout'); ?>>
+      <?php if (!is_front_page()) : ?>
+        <h1><?php the_title(); ?></h1> 
+      <?php endif; ?>
       <div>
         <?php the_content(); ?>
       </div>
